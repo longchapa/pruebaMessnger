1d45863c7943716947105398dcd3639228906cd8
Alejandro Castellanos <alejandrocastellanos.cos@gmail.com>
2020-09-24 11:38:07 -0500

    Se agrega mensaje para devolver

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const
     bodyParser = require('body-parser'),
     app = express().use(bodyParser.json());
 
+const request = require('request');
+
 
 //Se define el puerto del servidor y mensaje de conexion
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
@@ -14,10 +16,19 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 app.post('/webhook', (req, res) => {
     let body = req.body;
 
-    if(body.object === 'page') {
+    if (body.object === 'page') {
         body.entry.forEach(function(entry) {
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
         });
 
         res.status(200).send('EVENT_RECEIVED');
@@ -33,7 +44,7 @@ app.get('/webhook', (req, res) => {
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
 
-    if(mode && token) {
+    if (mode && token) {
         if(mode === 'subscribe' && token === VERIFY_TOKEN) {
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
@@ -42,3 +53,41 @@ app.get('/webhook', (req, res) => {
         res.sendStatus(403);
     }
 });
+
+function handleMessage(sender_psid, received_message){
+    let response;
+
+    if (received_message.text) {
+        response = {
+            "text" : `Enviaste el mensaje "${received_message}". Ahora envia una imagen!`
+        }
+    }
+
+    callSendAPI(sender_psid, response)
+}
+
+function handlePostback(sender_psid, received_postback){
+
+}
+
+function callSendAPI(sender_psid, response){
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": {"access_token": process.env.PAGE_ACCESS_TOKEN},
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.log('Unable to send message: ' + err)
+        }
+    });
+}
