1b37b67624c52e258db580489516e3b4e1ccb190
Alejandro Castellanos <alejandrocastellanos.cos@gmail.com>
2020-09-24 11:45:00 -0500

    Se agrega mensaje para devolver

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function callSendAPI(sender_psid, response){
         "json": request_body
     }, (err, res, body) => {
         if (!err) {
-            console.log('message sent!')
+            console.log('message sent!' + request_body)
         } else {
             console.log('Unable to send message: ' + err)
         }
